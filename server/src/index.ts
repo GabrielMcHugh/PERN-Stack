23e78a1cfa5ee79e4fb155386f493b937291f96a
LEAP\Gabriel.McHugh <gabriel.t.mchugh@gmail.com>
2023-04-03 10:58:17 +1000

    setup server

---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import {json, urlencoded } from 'body-parser'
+
+const app = express();
+
+const port = 9001;
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({extended: true}))
+
+app.listen(port, () => {
+    console.log(`Server ready at http://localhost${port}`)
+})
+
+app.get("/", (_, res) => {
+    res.send("Hello world!")
+})
+
+export default app;
